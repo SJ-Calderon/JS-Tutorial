c893da8ff11ab1f085e8535cb3a1ac19d38eaa93
Steven Calderon <stevencalderonjr@gmail.com>
2025-12-31 17:55:46 -0500

    HTML in JS

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,8 @@
-document.write("Hello World!");
+/* 
+    document.write() is depreciated
+    Instead, use one of the following:
+    document.getElementById(id).innerHTML -> Allow you to completely overwrite the HTML element
+    document.getElementById(id).innerText -> Maintains HTML tag, but overwrites text
+*/
+document.getElementById("header-h1").innerText = "This uses innerText to change the text of a header 1";
+document.getElementById("p1").innerHTML = "<h3>This uses innerHTML to change a paragraph into a header 3</h3>"
