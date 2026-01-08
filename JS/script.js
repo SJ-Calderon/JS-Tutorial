f28b30bdd804fd6f1a2e7b55e11258cf82890c48
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-08 17:56:49 -0500

    Variables and Data Types

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,25 @@
-/* 
-    document.write() is depreciated
-    Instead, use one of the following:
-    document.getElementById(id).innerHTML -> Allow you to completely overwrite the HTML element
-    document.getElementById(id).innerText -> Maintains HTML tag, but overwrites text
-*/
-document.getElementById("header-h1").innerText = "This uses innerText to change the text of a header 1";
-document.getElementById("p1").innerHTML = "<h3>This uses innerHTML to change a paragraph into a header 3</h3>"
+/*
+ * There are various ways to define a variable in JavaScript
+ * Older/Not Recommended: Automatically or Use var
+ * Newer/Recommended: Use let or const
+ * let: Block Scope, Declared before use, Cannot be Redeclared
+ * const: Block Scope, Cannot be Redeclared, Cannot be Reassigned
+ */
+
+// String
+let name = "Mike";
+
+// Number
+let age = 23;
+let gpa = 3.1;
+
+// Boolean
+let isMale = true;
+
+// Null - Object abscence
+const flaws = null;
+
+// Undefined - No assigned value
+let description = undefined;
+
+document.getElementById("p1").innerText = ("" + name + " is " + age + " years old and has a " + gpa + " gpa.");
