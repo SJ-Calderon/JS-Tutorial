a1dd71a5b52058243ddf639a12b4d4cf4117b011
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-09 19:01:34 -0500

    User Input

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,8 @@
-// Math Operators: +, -, *, /, %
+// window represents the browser's window
+// prompt displays a pop-up with a textbox
+// The pop-up also comes with "OK" and "Cancel" buttons
+let name = window.prompt("What is your name?");
 
-document.getElementById("p1").innerText = ( 2 + 5 * 2 / 7 % 3 );
+document.getElementById("p1").innerText = "Hey " + name + ". How are you today?";
 
-// Math Methods
-let num1 = -2;
-let num2 = 3;
-
-// abs, min, max
-let res = Math.abs(num1) + Math.min(num1,num2) + Math.max(num1,num2);
-
-// round
-// rounds down if the decimal is .4 or less and up if the decimal is .5 or more
-let num3 = 1.5;
-res += Math.round(num3);
-
-// pow
-res = Math.pow(res, 2);
-
-// sqrt
-res = Math.sqrt(res);
-
-// random
-let rand = Math.random();
-
-document.getElementById("p2").innerText = res + rand;
+document.getElementById("p2").innerText = "";
