3d04ed6d48e854cf35aa0415286d37ff4e737ea1
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-09 19:52:35 -0500

    Arrays

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,14 @@
-// window.prompt returns strings
-// To get the number, you must use either parseInt or parseFloat
-let num1 = parseFloat(window.prompt("Input a number"));
-let num2 = parseFloat(window.prompt("Input another number"));
+// Arrays can be created using the new keyword or using a literal
+let fruits = new Array("Apples", "Oranges", "Peaches");
+let cars = ["Saab", "Volvo", "BMW"];
 
-document.getElementById("p1").innerText = "Sum = " + (num1 + num2);
+// length property returns the number of array elements
+let numCarBrands = cars.length;
 
-document.getElementById("p2").innerText = "";
+// Strings can be split into an array using the split method
+let names = "Steven, Mike, Marc";
+names = names.split(",");
+
+document.getElementById("p1").innerText = "Random Fruit: " + fruits[Math.floor(Math.random()*10)%3];
+
+document.getElementById("p2").innerText = "Examples of names: " + names;
