d792066ea87aa6c18f1d1bbcc6e3dccd6ce2fdbb
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-09 19:14:57 -0500

    Basic Calculator

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,8 @@
-// window represents the browser's window
-// prompt displays a pop-up with a textbox
-// The pop-up also comes with "OK" and "Cancel" buttons
-let name = window.prompt("What is your name?");
+// window.prompt returns strings
+// To get the number, you must use either parseInt or parseFloat
+let num1 = parseFloat(window.prompt("Input a number"));
+let num2 = parseFloat(window.prompt("Input another number"));
 
-document.getElementById("p1").innerText = "Hey " + name + ". How are you today?";
+document.getElementById("p1").innerText = "Sum = " + (num1 + num2);
 
 document.getElementById("p2").innerText = "";
