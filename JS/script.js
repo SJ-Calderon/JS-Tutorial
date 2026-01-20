3e66d0957f602a4106eb39568f06e3553cb08cce
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-20 18:23:48 -0500

    Accessing HTML Elements

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,13 @@
-// Function syntax in JavaScript:
-// function funcName(params){}
-function sayHi(name, age){
-    document.getElementById("p1").innerText = "Hello " + name + ". You are " + age + ".";
-}
-let name = window.prompt("Type your name");
-let age = window.prompt("Type your age");
-sayHi(name,age);
+// This will store the HTML element in the variable
+let header = document.getElementById("header");
+let link = document.getElementById("link");
 
-function addition(num1, num2){
-    return num1 + num2;
-}
-let num1 = parseFloat(window.prompt("Input 1st number"));
-let num2 = parseFloat(window.prompt("Input 2nd number"));
-let sum = addition(num1, num2);
+// innerHTML will let you modify the HTML code for the element
+header.innerHTML = "Overwritten Header";
+link.innerHTML = "Amazon";
 
-document.getElementById("p2").innerText = "Sum of numbers is " + sum;
+// You can modify any attribute directly for the HTML element
+header.style.color = "red";
+header.style.backgroundColor = "blue";
+link.href = "https://www.amazon.com";
+link.style = "color:green;"
