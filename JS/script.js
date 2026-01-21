3bd36873744b7509d9d79ee9b94ec99dddfe3972
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-20 19:01:16 -0500

    Basic Messaging App

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,17 @@
-// This is the function which will be triggered with the onClick event
-function handleClick(element) {
-    element.style = "background-color:blue;"
-}
+let msgs = document.getElementById("messages");
+let txtbox = document.getElementById("textbox");
+let button = document.getElementById("button");
 
-let img = document.getElementById("image");
+button.addEventListener("click", function(){
+    // createElement will create a new HTML element
+    let newMsg = document.createElement("li");
 
-// This will add a listener for the specified event
-// You can specify the code directly in the parameter
-// You need to drop the "on" for the event when using it directly in JS
-img.addEventListener("mouseover", function(){
-    this.style = "box-shadow: 2px 2px 2px grey;";
-    this.width = "150";
-});
+    // The value attribute will grab the text within the HTML element
+    newMsg.innerHTML = txtbox.value;
 
-// This will reset the image to the original styling
-img.addEventListener("mouseout", function(){
-    this.style = "";
-    this.width = "100";
+    // appendChild will add the given HTML element as a child
+    msgs.appendChild(newMsg);
+
+    // This clears the text within the textbox
+    txtbox.value = "";
 });
