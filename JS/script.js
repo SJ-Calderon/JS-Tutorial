8bdb60aa29ed36d6397f61c2002b3389135ccede
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-20 21:56:19 -0500

    Multiple Choice Quiz

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,36 @@
 let p1 = document.getElementById("p1");
 let p2 = document.getElementById("p2");
 
-for (let i = 0; i < 10; i++) {
-    p1.innerHTML += i + "<br />";
+let questions = [
+    {
+        prompt: "What color are apples?\n(a) Red/Green\n" +
+        "(b) Purple\n(c) Orange",
+        answer: "a"
+    },
+    {
+        prompt: "What color are Bananas?\n(a) Teal\n" +
+        "(b) Magenta\n(c) Yellow",
+        answer: "c"
+    },
+    {
+        prompt: "What color are strawberries?\n(a) Yellow\n" +
+        "(b) Red\n(c) Blue",
+        answer: "b"
+    }
+];
+
+let score = 0;
+
+for (let i = 0; i < questions.length; i++) {
+    let response = window.prompt(questions[i].prompt);
+
+    if (response == questions[i].answer) {
+        score++;
+        alert("Correct!")
+    }
+    else {
+        alert("WRONG!");
+    }
 }
 
-let friends = ["Jim", "Stanley", "Kevin"];
-
-for (let j = 0; j < friends.length; j++) {
-    p2.innerHTML += friends[j] + "<br />";
-}
+alert("You got " + score + "/" + questions.length);
