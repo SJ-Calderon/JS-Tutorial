d9cfec4b13ba185c62d7d7a94f3be25a574f30d1
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-20 22:47:23 -0500

    Timing

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,3 @@
-let p1 = document.getElementById("p1");
-let p2 = document.getElementById("p2");
-
-let friends = ["Oscar", "Angela", "Kevin"];
-
-// forEach specifies what code will run for each of the elements of the array
-friends.forEach(function(element) {
-    p1.innerText += element + "\n";
-});
-
-let books = [
-    {
-        title: "Harry Potter",
-        author: "JK Rowling",
-        pages: 300
-    },
-    {
-        title: "Green Eggs & Ham",
-        author: "Dr. Seuss",
-        pages: 25
-    }
-];
-
-books.forEach(function(book) {
-    p2.innerText += "Author: " + book.author + "\n";
-});
+function sayHi() {
+    alert("Hi");
+}
