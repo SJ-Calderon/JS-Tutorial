5af3aa9cec70dea66d9b780759c6991ef2eedce2
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-20 22:59:40 -0500

    Web Storage

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,12 @@
-function sayHi() {
-    alert("Hi");
-}
+let p1 = document.getElementById("p1");
+
+let txtbox = document.getElementById("textbox");
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    // localStorage is where you can store things locally across sessions
+    // You can specify any attribute that you want to store
+    localStorage.name = txtbox.value;
+});
+
+p1.innerText = localStorage.name;
