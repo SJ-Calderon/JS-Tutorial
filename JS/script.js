4d7196869e039bbcfec99b626d264c17ea364440
Steven Calderon <stevencalderonjr@gmail.com>
2026-01-20 21:20:12 -0500

    While Loops

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,14 @@
 let p1 = document.getElementById("p1");
 let p2 = document.getElementById("p2");
 
-let myMovie = {
-    title: "The Social Network",
-    releaseYear: "2010",
-    duration: 2.0,
-    actors: [
-        {
-            name: "Jessie Eisenburg",
-            birthday: new Date("October 5, 1983"),
-            hometown: "New York, New York"
-        },
-        {
-            name: "Ronney Mara",
-            birthday: new Date("April 17, 1985"),
-            hometown: "Bedford, New York"
-        }
-    ]
-};
+let i = 1;
+let j = 20;
 
-p1.innerText = myMovie.title;
-p2.innerText = myMovie.actors[0].name;
+while (i <= 10) {
+    p1.innerHTML += i + "<br />";
+    i++;
+}
+
+do {
+    p2.innerHTML += j + "<br />";
+} while (j <= 10)
